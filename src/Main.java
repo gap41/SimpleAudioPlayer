4024d7c5bb5c52b54fe362716fb883ee1823f200
gap41 <william@varholm.no>
2024-06-13 13:37:23 +0200

    Added the usual

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.io.*;
+import java.util.*;
+import javax.sound.sampled.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+        File file = new File("Red.wav");
+
+        Scanner scanner = new Scanner(System.in);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+        String response = "";
+
+        while(!response.equals("Q")){
+            System.out.println("P = Play, S = Stop, R = Reset, Q = Quit");
+            System.out.print("Enter your choice: ");
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch(response){
+                case ("P") : clip.start(); break;
+                case ("S") : clip.stop(); break;
+                case ("R") : clip.setMicrosecondPosition(0); break;
+                case ("Q") : response = "Q"; break;
+                default: System.out.println("Not a valid response");
+            }
+        }
+
+
     }
 }
